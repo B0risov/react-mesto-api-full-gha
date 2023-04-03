f00ad1465adf74badf5952d82ca7494af26bac95
Borisov <sendetskymatvei@yandex.ru>
2023-04-03 19:25:14 +0400

    test 5

--- backend/constants.js
+++ backend/constants.js
@@ -3,12 +3,12 @@ const regexUrl = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w.-]*)*\/?$/;
 const allowedCors = [
   'https://praktikum.tk',
   'https://praktikum.tk',
-  'https://localhost:3000',
-  'https://localhost:3000',
-  'https://158.160.32.56:3000',
+  'https://localhost:80',
+  'https://localhost:80',
+  'https://158.160.32.56:80',
   'https://B0risov.nomoredomains.work',
   'https://api.B0risov.students.nomoredomains.monster',
-  'localhost:3000',
+  'localhost:80',
 ];
 
 module.exports = {
