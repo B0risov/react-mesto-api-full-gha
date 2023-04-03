34bb7df2a620b3afba1c1a4511caf54340b56441
Borisov <sendetskymatvei@yandex.ru>
2023-04-03 18:48:14 +0400

    test 4

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 ## Ссылки на проект
 
-IP-адрес 158.160.32.56
+IP 158.160.32.56
 
 Frontend https://B0risov.nomoredomains.work
 
--- backend/constants.js
+++ backend/constants.js
@@ -2,10 +2,10 @@ const regexUrl = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w.-]*)*\/?$/;
 
 const allowedCors = [
   'https://praktikum.tk',
-  'http://praktikum.tk',
+  'https://praktikum.tk',
   'https://localhost:3000',
-  'http://localhost:3000',
-  'http://158.160.32.56:3000',
+  'https://localhost:3000',
+  'https://158.160.32.56:3000',
   'https://B0risov.nomoredomains.work',
   'https://api.B0risov.students.nomoredomains.monster',
   'localhost:3000',
